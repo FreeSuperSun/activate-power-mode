198e126b10b8f69765141ff8d9cbd9a6f6139dc7
FreeSuperSun <freesupersun@freesupersun.com>
2020-11-22 17:52:58 +0800

    '修改绘制的方式,去掉设置标志,而是完全通过数组长度来判断.同时不再绘制的彩弹会被删除,防止长时间占用参与循环.'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+//震动窗口
+function shakeWindow() {
+    //随机指定系数 1-3
+    let intensity = 1 + 2 * Math.random();
+    //随机指定方向
+    let x = intensity * (Math.random() > 0.5 ? -1 : 1);
+    let y = intensity * (Math.random() > 0.5 ? -1 : 1);
+    //设置偏移
+    document.body.style.marginLeft = x + 'px';
+    document.body.style.marginTop = y + 'px';
+    //计时器恢复原来的位置
+    setTimeout(function () {
+        document.body.style.marginLeft = '';
+        document.body.style.marginTop = '';
+    }, 50);
+}
+
 //绘制彩弹的画布初始化
 function initCanvas() {
     let canvas = document.createElement('canvas');
@@ -18,14 +35,11 @@ let canvas = initCanvas();
 //canvas的上下文
 //TODO canvas相关
 let canvasContext = canvas.getContext('2d');
-//彩弹的数组
+
+//彩弹的数组 全局存放,全局显示
+//TODO 但是这样也意味着,会有一个500项的数组一直存在内存里
+//小球画完,其实是不删的,这样处理应该不太好.
 let particles = [];
-//操作每个彩弹的位置变量
-let particlePointer = 0;
-//TODO 是否渲染? 标识是否正在渲染
-let rendering = false;
-//设置是否震动窗口
-POWERMODE.shake = true;
 
 //随机数,包含min 不包含max
 function getRandom(min, max) {
@@ -90,7 +104,7 @@ function createParticle(x, y, color) {
     };
 }
 
-POWERMODE.colorful = false;
+POWERMODE.colorful = true;
 
 //重绘函数
 function loop() {
@@ -102,12 +116,16 @@ function loop() {
     let rendered = false;
     let rect = canvas.getBoundingClientRect();
     //TODO for循环不是可以用const吗?
-    //对每个小球进行绘制
+    //对每个小球进行绘制,一帧绘制一次
     for (let i = 0; i < particles.length; i++) {
         let particle = particles[i];
         //如果小球的alpha到了0.1,就再不继续绘制下一次了,就消失了
-        if (particle.alpha <= 0.1)
+        if (particle.alpha <= 0.1) {
+            particles.splice(i, 1);
+            //索引-1,否则会因为用了splice导至跳过元素
+            i--;
             continue;
+        }
         // 垂直方向速度减小,模拟抛物到下落的过程,应该最终的效果是1秒9.8
         //TODO 是怎么控制刷新频率的?
         particle.velocity.y += 0.075;
@@ -132,37 +150,36 @@ function loop() {
     }
     //如果上次还画过小球,就进行下一次递归
     if (rendered) {
+        //TODO requestAnimationFrame,其实就是要在下一帧调用回调函数
+        // 可以用来制作动画效果
         requestAnimationFrame(loop);
     } else {
-        //否则,就把rendering设为false,表示不在渲染中.
-        rendering = false;
     }
 }
 
 
 //彩弹
 function POWERMODE() {
-    { // spawn particles
+    {
         //取得光标的位置
         let caret = getCaret();
         //设置彩弹的数量 0到15个
         let numParticles = 5 + Math.round(Math.random() * 10);
-        //设置每个彩弹
-        while (numParticles--) {
+        //设置每个彩弹||
+        //TODO 这里为什么要用一个全局的变量来存储彩弹呢?
+        //TODO 因为这样的话可以让多次按键的彩弹同时存在
+        for (let i = 0; i < numParticles && particles.length < 500; i++) {
             //给数组里面填充彩弹
-            particles[particlePointer] = createParticle(caret.x, caret.y, caret.color);
-            //这里应该是设置彩弹上限的,最多500个
-            //这里应该没用吧,在上面设置彩弱的时候注意就行了
-            particlePointer = (particlePointer + 1) % 500;
+            particles.push(createParticle(caret.x, caret.y, caret.color));
+            //注意这里是设置的全屏的彩弹数量,而不是按一次的彩弹数量
+            // particlePointer = (particlePointer + 1) % 500;
         }
-    }
-    { // shake screen
-        if (!rendering) {
-            //TODO 重绘?
+        if (particles.length === numParticles) {
+            //TODO 如果正在渲案中,就没必要再加添loop,因为彩弹已经加到数组里了,也会进行绘制.
             requestAnimationFrame(loop);
         }
     }
 }
 
 //导出模块
-module.exports = {POWERMODE: POWERMODE};
+module.exports = {POWERMODE, shakeWindow};
